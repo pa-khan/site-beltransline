55a6f596295149b8333ca3c6e865cda133c86ec3
e5ash <io@e5ash.com>
2018-04-20 02:14:12 +0300

    Desktop 100%

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -1,5 +1,35 @@
 $(document).ready(function($) {
 
-	$('.text_phone').mask('+7 (999) 999-99-99');
+	$('.input_tel input').mask('+7 (999) 999-99-99');
 	
+	$('.search__btn-show').click(function(event) {
+		$('.search__wrap').addClass('search__wrap_toggle');
+		$(this).hide();
+	});
+
+	$('.clients__list').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1
+	})
+
+
+	var portfolioImg = $('.portfolio__img'),
+			portfolioImgs = $('.portfolio__imgs');
+
+	portfolioImg.slick({
+		asNavFor: portfolioImgs
+	});
+
+	portfolioImgs.slick({
+		asNavFor: portfolioImg,
+		slidesToShow: 7,
+		arrows: false
+	});
+
+
+	$('.team__list').slick({
+		slidesToShow: 2
+	})
+
+	$(".nano").nanoScroller();
 });
